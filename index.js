2f2394b1dc7a9c272bf74487d22b93cfd8e1d649
sannypro <sannymr79@gmail.com>
2022-05-16 08:55:53 +0600

    admin route done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -12,7 +13,20 @@ app.get('/', (req, res) => {
 })
 
 
-
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbbiden access' })
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f1eax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
@@ -23,6 +37,7 @@ async function run() {
         console.log('Db is connected');
         const servicesCollection = client.db("doctors_portal").collection("services");
         const bookingCollection = client.db("doctors_portal").collection("booking");
+        const userCollection = client.db("doctors_portal").collection("user");
         app.get("/service", async (req, res) => {
             const query = {}
             const cursor = servicesCollection.find(query)
@@ -31,6 +46,7 @@ async function run() {
         });
         app.post('/booking', async (req, res) => {
             const booking = req.body;
+
             console.log(booking);
             const query = {
                 treatment
@@ -61,6 +77,63 @@ async function run() {
                 service.slots = available
             })
             res.send(services)
+        })
+        app.get('/users', verifyToken, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users)
+
+        })
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
+        app.put('/user/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === "admin") {
+                const filter = { email: email }
+
+                const updatedDoc = {
+                    $set: { role: 'admin' }
+                }
+                const result = await userCollection.updateOne(filter, updatedDoc);
+                const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'Forbbiden' });
+            }
+
+        })
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email
+            console.log(email);
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+        app.get('/dashboard', verifyToken, async (req, res) => {
+            const patientEmail = req.query.patientEmail;
+            const decodeEmail = req.decoded.email;
+            if (patientEmail === decodeEmail) {
+                const query = { patientEmail: patientEmail };
+                const bookings = await bookingCollection.find(query).toArray();
+                res.send(bookings)
+            }
+            else {
+                return res.status(403).send({ message: 'Forbbiden access' })
+            }
+
+
         })
     }
     finally {
